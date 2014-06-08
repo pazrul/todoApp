cfa05fabb8d84dcbefcc867dd09bdc3f2a8cf2b8
Jeremy Whitbred <pazrul@gmail.com>
2014-06-08 12:06:41 -0700

    Fix Jade indentation, REALLY add pretty print html

--- app.js
+++ app.js
@@ -17,10 +17,13 @@ var db = monk(URI);
 var app = express();
 
 // all environments
+app.use(function(req, res, next) {
+  app.locals.pretty = true;
+  next();
+});
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.set('view options', { pretty: true });
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
